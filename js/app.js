3d0746f5c64de0ebcda756d5a95fcb032ddedbd1
Kamoullox <boucetta.kamel62@gmail.com>
2020-05-19 17:31:34 +0200

    MAJ Ajout de citations

--- js/app.js
+++ js/app.js
@@ -15,7 +15,22 @@ let quotes =
     "Chuck Norris a déjà compté jusqu'à l'infini. Deux fois.",
     "Chuck Norris sait coder en binaire avec que des 0.",
     "Chuck Norris peut taguer le mur du son.",
-    "Si Chuck Norris attaque l'Empire, l'Empire ne contre attaque pas."
+    "Si Chuck Norris attaque l'Empire, l'Empire ne contre attaque pas.",
+    "Chuck Norris nettoie Mr Propre.",
+    "Google, c'est le seul endroit où tu peux taper Chuck Norris...",
+    "Chuck Norris est contre les radars automatiques : ça l'éblouit lorsqu'il fait du vélo. ",
+    "Chuck Norris n'utilise pas de suppositoire. Les voies du seigneur sont impénétrables.",
+    "Chuck Norris mange les emballages des carambars. On ne blague pas avec Chuck Norris.",
+    "Chuck Norris peut remonter le terrain de foot dans Olive et Tom en moins d'un épisode.",
+    "Chuck Norris a fini Super Mario Bros sans sauter.",
+    "Chuck Norris est capable de laisser un message avant le bip sonore.",
+    "Maitre Yoda a perdu la vie le jour où il a appelé Chuck Norris \"Norris Chuck\".",
+    "Chuck Norris a fini son forfait illimité.",
+    "Un film de Bruce Lee montre Chuck Norris se faire battre par Bruce Lee. C'est là l'effet spécial le plus cher de toute l'histoire du cinéma.",
+    "Chuck Norris a fini Pokemon sans AUCUN pokemon. Chuck Norris n'a besoin de personne.",
+    "Chuck Norris peut monter en bas.",
+    "Un jour, les Power Rangers ont rencontré Chuck Norris. Maintenant on les appelle les Télétubbies."
+
 ];
 
 let body = document.querySelector("body");
@@ -27,14 +42,19 @@ btn.addEventListener("click",magic);
 
 function magic() {
     let indexQuote = Math.floor(Math.random()*quotes.length);
-
-    let colorR = Math.floor(Math.random()*256);
-    let colorG = Math.floor(Math.random()*256);
-    let colorB = Math.floor(Math.random()*256);
-
-    body.style.backgroundColor = `rgb(${colorR},${colorG}, ${colorB})`;
-    btn.style.backgroundColor = `rgb(${colorR},${colorG}, ${colorB})`;
-    quoteText.style.color = `rgb(${colorR},${colorG}, ${colorB})`;
-    
+    setColor();    
     quoteText.textContent = quotes[indexQuote];
+    console.log("Coucou")
+;}
+
+function setColor(){
+    let color = [];
+    for (let i=0; i<3; i++) {
+        color.push(Math.floor(Math.random()*256));
+    }
+    color = `rgb(${color[0]},${color[1]}, ${color[2]})`;
+    body.style.backgroundColor = color;
+    btn.style.backgroundColor = color;
+    quoteText.style.color = color;
 }
+
